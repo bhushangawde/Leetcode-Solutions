9099bf5898497164614dcbed17cf67ef750a0c98
bhushangawde <bhushangawde32@gmail.com>
2021-07-20 21:03:44 +0530

    Updated 122

--- 122_Best_time_buy_sell_stock_2.cpp
+++ 122_Best_time_buy_sell_stock_2.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    
     int maxProfit(vector<int>& prices) {
         int profit = 0;
         int i = 0;
@@ -31,4 +32,18 @@ public:
         
         return profit;
     }
+    
+    // Another solution
+	
+    // int maxProfit(vector<int>& prices) {
+    //     int profit = 0;
+    //     for(int i = 1 ; i < prices.size(); i++){
+    //         if(prices[i] > prices[i-1])
+    //             profit += prices[i] - prices[i-1];
+    //     }    
+    //     return profit;
+    // }
+    
+    
+    
 };
