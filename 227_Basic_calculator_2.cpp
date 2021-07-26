7a9eb65d347d35aa7066796914ce93cb832d2f19
bhushangawde <bhushangawde32@gmail.com>
2021-07-26 23:05:43 +0530

    Added 227

---
+++ 227_Basic_calculator_2.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    int calculate(string s) {
+        int i = 0;
+        int result = 0;
+        stack <int> nums, ops;
+        int num = 0;
+        int sign = 1;
+        while(i < s.size()){
+            char c = s[i];
+            if(isdigit(c)){
+                num = num * 10 + (c - '0');
+                // cout<<num<<endl;
+                i++;
+            }
+            else if (c == '-'){
+                result += num*sign;
+                num = 0;
+                sign = -1;
+                i++;
+            }
+            else if (c == '+'){
+                result += num*sign;
+                num = 0;
+                sign = 1;
+                i++;
+            }
+            else if (c == '*'){
+                int num2 = 0;
+                i++;
+                while(!isdigit(s[i]))
+                    i++;
+                while(isdigit(s[i])){
+                    num2 = num2 * 10 + (s[i] - '0');
+                    i++;
+                }
+                num = num*num2*sign;
+                sign = 1;
+                
+            }
+            else if (c == '/'){
+                int num2 = 0;
+                i++;
+                while(!isdigit(s[i]))
+                    i++;
+                while(isdigit(s[i])){
+                    num2 = num2 * 10 + (s[i] - '0');
+                    i++;
+                }
+                num = num/num2*sign;
+                sign = 1;
+            }
+            else
+                i++;
+        }
+        result += num*sign;
+        return result;
+    }
+};
