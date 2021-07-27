e440fd1393bc783931bc8ca508a1dfd7649b739a
bhushangawde <bhushangawde32@gmail.com>
2021-07-27 18:32:16 +0530

    Added 350

---
+++ 350_Intersection_of_2arrays_2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        int mp1[1001] = {0}, mp2[1001] = {0};
+        vector<int> ans;
+        for(int i = 0 ; i< nums1.size(); i++){
+            mp1[nums1[i]]++;
+        }
+        for(int i = 0 ; i< nums2.size(); i++){
+            mp2[nums2[i]]++;
+        }
+        
+        for(int i = 0 ; i < 1001; i++){
+            if(mp1[i] > 0 && mp2[i] > 0){
+                int c = min(mp1[i] , mp2[i]);
+                for(int j = 0 ; j < c ; j++)
+                    ans.push_back(i);
+            }
+        }
+        
+        return ans;
+    }
+};
