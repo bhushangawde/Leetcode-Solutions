f3a5013b7144b3337b63d6cdb47cb9c3fc4da2cb
bhushangawde <bhushangawde32@gmail.com>
2021-07-27 18:37:30 +0530

    Updated solution 350

--- 350_Intersection_of_2arrays_2.cpp
+++ 350_Intersection_of_2arrays_2.cpp
@@ -20,4 +20,21 @@ public:
         
         return ans;
     }
+	
+	
+	// Another Solution
+/* 	vector<int> intersect(vector<int>& a, vector<int>& b) {
+		unordered_map<int, int> ctr;
+		for (int i : a)
+			ctr[i]++;
+		vector<int> out;
+		for (int i : b)
+			if (ctr[i]-- > 0)
+				out.push_back(i);
+		return out;
+ 	}
+	*/
+	
+	
+	
 };
