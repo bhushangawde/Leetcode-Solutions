fc1b27e0446dcd0657e56f2bf080e66f23cf21e3
bhushangawde <bhushangawde32@gmail.com>
2021-07-31 22:59:57 +0530

    Added 235 new

--- 235_LCA_of_BST.cpp
+++ 235_LCA_of_BST.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+
+	// Iterative
+	/* TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+        while(root!=NULL){
+            if(root->val > p->val && root->val> q->val)
+                root = root->left;
+            
+            else if(root->val < p->val && root->val < q->val)
+                root = root->right;
+            
+            else
+                break;    
+        }
+        return root;
+    } */
+	
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         TreeNode* x=root;
         if((x->val>=q->val && x->val<=p->val) || (x->val>=p->val && x->val<=q->val)){
