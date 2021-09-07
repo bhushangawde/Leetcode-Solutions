3281faf450df1e2a3b58f36c49e54521610e9a3c
bhushangawde <bhushangawde32@gmail.com>
2021-09-07 23:38:57 +0530

    Added solution 540

---
+++ 540_Single_element_in_sorted_array.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& arr) {
+        int l = 0;
+        int size = arr.size();
+        int r = size - 1;
+        int mid;
+        
+        if(size == 1 || arr[0] != arr[1])
+            return arr[0];
+        
+        if(arr[size-1] != arr[size-2])
+            return arr[size-1];
+        
+        while(l<=r){
+            mid = (l + r)/2;
+            
+            if((mid + 1 < size && arr[mid] != arr[mid + 1]) && (mid - 1 > -1 && arr[mid] != arr[mid - 1]))
+                return arr[mid];
+            
+            else if((mid + 1 < size && arr[mid] == arr[mid + 1] && mid%2 == 0)|| (mid - 1 > -1 && arr[mid] == arr[mid - 1] && mid%2 == 1)){
+                l = mid + 1;
+            }               
+            else{
+                r = mid - 1;
+            }
+        }
+        
+        return -1;
+    }
+};
