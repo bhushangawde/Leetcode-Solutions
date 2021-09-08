8a62f8dda3e743df1136cbf53f01f1944556aa5b
bhushangawde <bhushangawde32@gmail.com>
2021-09-08 21:52:27 +0530

    Updated solution 347

--- 347_top_k_frequent_elements.cpp
+++ 347_top_k_frequent_elements.cpp
@@ -1,26 +1,46 @@
 class Solution {
 public:
-    typedef pair<int,int> pr;
+    // typedef pair<int,int> pr;
     
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+//         vector<int> ans;
+//         priority_queue<pr, vector<pr>, greater<pr>> pq;
+//         unordered_map<int, int> umap;
+        
+//         for(int i = 0 ; i< nums.size(); i++){
+//             umap[nums[i]]++;
+//         }
+        
+//         for(auto it = umap.begin(); it!= umap.end(); it++){
+//             pq.push({it->second, it->first});
+//             if(pq.size() > k)
+//                 pq.pop();
+//         }
+        
+//         while(pq.size() > 0){
+//             ans.push_back(pq.top().second);
+//             pq.pop();
+//         }
+//         return ans;
+//     }
     vector<int> topKFrequent(vector<int>& nums, int k) {
         vector<int> ans;
-        priority_queue<pr, vector<pr>, greater<pr>> pq;
+        priority_queue<pair<int,int>> pq;
         unordered_map<int, int> umap;
         
-        for(int i = 0 ; i< nums.size(); i++){
-            umap[nums[i]]++;
+        for(auto i : nums){
+            umap[i]++;
         }
         
-        for(auto it = umap.begin(); it!= umap.end(); it++){
-            pq.push({it->second, it->first});
-            if(pq.size() > k)
-                pq.pop();
+        for(auto i : umap){
+            pq.push({i.second, i.first});
         }
         
-        while(pq.size() > 0){
+        while(k--){
             ans.push_back(pq.top().second);
             pq.pop();
         }
         return ans;
     }
+    
 };
