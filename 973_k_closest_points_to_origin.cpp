4b069fb040bb8e9bcc98c0d5c27d20bc7a303468
bhushangawde <bhushangawde32@gmail.com>
2021-09-10 00:29:56 +0530

    Added 973 quickselect

--- 973_k_closest_points_to_origin.cpp
+++ 973_k_closest_points_to_origin.cpp
@@ -1,22 +1,63 @@
 class Solution {
 public:
     
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<pair<int,vector<int>>> maxh;
-        vector<vector<int>> ans;
+//     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+//         priority_queue<pair<int,vector<int>>> maxh;
+//         vector<vector<int>> ans;
         
-        for(int i = 0 ; i < points.size(); i++){
-            maxh.push({points[i][0]*points[i][0] + points[i][1]*points[i][1], points[i]});
+//         for(int i = 0 ; i < points.size(); i++){
+//             maxh.push({points[i][0]*points[i][0] + points[i][1]*points[i][1], points[i]});
             
-            if(maxh.size()>k)
-                maxh.pop();
+//             if(maxh.size() > k)
+//                 maxh.pop();
+//         }
+        
+//         while(maxh.size() > 0){
+//             ans.push_back(maxh.top().second);
+//             maxh.pop();
+//         }
+        
+//         return ans;
+//     }
+    
+	
+	// QUICKSELECT IMPLEMENTATION
+    int distance(int x, int y){
+        return x*x + y*y;
+    }
+    
+    int partition(vector<vector<int>>& points, int low, int high, int k){
+        int idx = low;
+        int pivot = distance(points[high][0],points[high][1]);
+        
+        for(int j = low; j < high; j++){
+            if(distance(points[j][0], points[j][1]) < pivot){
+                swap(points[idx], points[j]);
+                idx++;
+            }
+        }
+        swap(points[idx], points[high]);
+        
+        return idx;
+    }
+    
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        
+        vector<vector<int>> ans;
+        int low = 0;
+        int high = points.size() - 1;
+        int mid;
+        
+        while(low <= high){
+            mid = partition(points, low, high, k);
+            if(mid == k)
+                break;
+            if(mid < k)
+                low = mid + 1;
+            else
+                high = mid - 1;
         }
         
-        while(maxh.size() > 0){
-            ans.push_back(maxh.top().second);
-            maxh.pop();
-        }
-        
-        return ans;
+        return vector<vector<int>>(points.begin(), points.begin() + k);
     }
 };
