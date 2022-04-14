f5141bac47aa96b57e614965d969644a7ee5c05a
bhushangawde <bhushangawde32@gmail.com>
2022-04-15 00:05:31 +0530

    Added simple solution

--- 12_Integer_to_Roman.cpp
+++ 12_Integer_to_Roman.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/* class Solution {
 public:
     string intToRoman(int num) {
         string s = "";
@@ -74,6 +74,24 @@ public:
             }
         }
     
+        return s;
+    }
+}; */
+
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        string s = "";
+        int values[13] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        string symbols[13] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        
+        for(int i = 0 ; i < 13; i++){
+            while(num >= values[i]){
+                s += symbols[i];
+                num = num - values[i];
+            }    
+        }
         return s;
     }
 };
