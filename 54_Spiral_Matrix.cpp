6bca562c8c1a8b4b192a7b25bf5eb7b9b178ae5a
bhushangawde <bhushangawde32@gmail.com>
2022-04-16 13:10:14 +0530

    Added 54 simple

---
+++ 54_Spiral_Matrix.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public
+    vectorint spiralOrder(vectorvectorint& matrix) {
+        int rowBegin = 0;
+        int colBegin = 0;
+        int rowEnd = matrix.size() - 1;
+        int colEnd = matrix[0].size() - 1;
+        vectorint ans;
+        
+        while(rowBegin = rowEnd  colBegin = colEnd){
+            if(rowBegin = rowEnd){
+                for(int i = colBegin; i = colEnd; i++){
+                    ans.push_back(matrix[rowBegin][i]);
+                }
+            }
+            rowBegin++;
+            
+            if(colBegin = colEnd){
+                for(int i = rowBegin; i = rowEnd; i++){
+                    ans.push_back(matrix[i][colEnd]);
+                }
+            }
+            colEnd--;
+            
+            if(rowBegin = rowEnd){
+                for(int i = colEnd; i = colBegin; i--){
+                    ans.push_back(matrix[rowEnd][i]);
+                }
+            }
+            rowEnd--;
+            
+            if(colBegin = colEnd){
+                for(int i = rowEnd; i = rowBegin; i--){
+                    ans.push_back(matrix[i][colBegin]);
+                }
+            }
+            colBegin++;
+        }
+        
+        return ans;
+    }
+};
