2254d35e88f26a5ed961336e9a14b54ab27a94ba
bhushangawde <bhushangawde32@gmail.com>
2022-04-24 13:26:09 +0530

    Added different approach

--- 102_Level_order_traversal.cpp
+++ 102_Level_order_traversal.cpp
@@ -42,4 +42,35 @@ public:
         }
         return ans;
     }
+	
+	
+	// Solution 2
+	
+	/* vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        queue<TreeNode*> q;
+        vector<int> vec;
+        if(!root)
+            return ans;
+        
+        q.push(root);
+        while(!q.empty()){
+            int size = q.size();
+            while(size > 0){
+                TreeNode *node = q.front();
+                q.pop();
+                vec.push_back(node->val);
+                if(node->left)
+                    q.push(node->left);
+                
+                if(node->right)
+                    q.push(node->right);
+                size--;
+            }
+            ans.push_back(vec);
+            vec.clear();
+        }
+        
+        return ans;
+    } */
 };
