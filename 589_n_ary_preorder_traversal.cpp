c1cee3dd63cfecf959b9b51475a5a5982136fb6e
bhushangawde <bhushangawde32@gmail.com>
2022-04-25 23:25:13 +0530

    Added iterative solution for 589 using stack

--- 589_n_ary_preorder_traversal.cpp
+++ 589_n_ary_preorder_traversal.cpp
@@ -36,4 +36,26 @@ public:
         preorderRecursive(root, ans);
         return ans;
     }
+	
+	//Iterative solution
+	
+	/* vector<int> preorder(Node* root) {
+        vector<int> ans;
+        if(!root){
+            return ans;
+        }
+        stack<Node*> st;
+        st.push(root);
+        
+        while(!st.empty()){
+            Node* node = st.top();
+            st.pop();
+            ans.push_back(node->val);
+            
+            for(int i = node->children.size() - 1; i >= 0; i--){
+                st.push(node->children[i]);
+            }
+        }
+        return ans;
+    } */
 };
