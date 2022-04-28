5d1db1a07e72a139cd1b6e30ff5abf5ed89b1001
bhushangawde <bhushangawde32@gmail.com>
2022-04-28 16:20:36 +0530

    Adding iterative + reverse

--- 590_n_ary_postorder_traversal.cpp
+++ 590_n_ary_postorder_traversal.cpp
@@ -36,4 +36,25 @@ public:
         postorderRecursive(root, ans);
         return ans;
     }
+	
+	//Iterative using stack and reverse function
+	/* vector<int> postorder(Node* root) {
+        if(root==NULL) 
+            return {};
+        vector<int> res;
+        stack<Node*> stk;
+        stk.push(root);
+        while(!stk.empty())
+        {
+            Node* temp=stk.top();
+            stk.pop();
+            for(int i=0;i<temp->children.size();i++){
+                stk.push(temp->children[i]);
+            }
+            res.push_back(temp->val);
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+	 */
 };
