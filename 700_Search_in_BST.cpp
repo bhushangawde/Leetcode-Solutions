1edb5c998b6ec765e4aa1867d61e8e96bfe0f7d6
bhushangawde <bhushangawde32@gmail.com>
2022-05-08 19:12:00 +0530

    Adding 700 simple iterative

--- 700_Search_in_BST.cpp
+++ 700_Search_in_BST.cpp
@@ -9,8 +9,28 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ 
+ 
+
 class Solution {
 public:
+	
+	// Adding the iterative solution
+	/* TreeNode* searchBST(TreeNode* root, int val) {
+        while(root!= NULL){
+            if(root->val == val){
+                return root;
+            }
+            if(root->val < val){
+                root = root->right;
+            }
+            else{
+                root = root->left;
+            }
+        }
+        return NULL;
+    } */
+	
     TreeNode* searchBST(TreeNode* root, int val) {
         if(root == NULL){
             return NULL;
