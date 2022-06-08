454dc2944c05f63077be1ba4f959f97c0b441ff2
bhushangawde <bhushangawde32@gmail.com>
2022-06-08 12:15:04 +0530

    O(1) space solution with simple maths

--- 6_Zigzag_conversion.cpp
+++ 6_Zigzag_conversion.cpp
@@ -1,6 +1,39 @@
 class Solution {
 public:
-    string convert(string s, int numRows) {
+
+
+	// O(1) space solution
+	string convert(string s, int numRows) {
+        if(numRows <= 1)
+            return s;
+        string ans;
+        int step1, step2, pos;
+        for(int i = 0; i < numRows; i++){
+            step1 = (numRows - i - 1)*2;
+            step2 = i*2;
+            pos = i;
+            if(pos < s.size())
+                ans += s[pos];
+            while(true){
+                pos += step1; 
+                if(pos >= s.size())
+                    break;
+                if(step1)
+                    ans += s[pos];
+                
+                
+                pos += step2;
+                if(pos >= s.size())
+                    break;
+                if(step2)
+                    ans += s[pos];
+            }    
+        }
+        return ans; 
+    }
+	
+	// O(n) space solution
+    /* string convert(string s, int numRows) {
         vector<string> rows(min(numRows, int(s.size())));
         string ans;
         int currRow = 0;
@@ -23,5 +56,5 @@ public:
         }
         
         return ans; 
-    }
+    } */
 };
