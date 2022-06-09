915a7c17921b13d99ffa2c57ef83b55d36263816
bhushangawde <bhushangawde32@gmail.com>
2022-06-09 10:42:08 +0530

    Swapping nodes instead of values

--- 1721_Swapping_Nodes_in_a_Linked_list.cpp
+++ 1721_Swapping_Nodes_in_a_Linked_list.cpp
@@ -37,4 +37,47 @@ public:
         
         return head;
     }
+	
+	
+	// Swapping nodes instead of values
+	/* ListNode* swapNodes(ListNode* head, int k) {
+        ListNode *dummy = new ListNode(-1);
+        dummy->next = head;
+        
+        ListNode *kth_begin = dummy;
+        ListNode *kth_end = dummy;
+        ListNode *node = dummy;
+        ListNode *prev_1 = dummy;
+        ListNode *prev_2 = dummy;
+        
+        
+        int i = 0;
+        while(i < k){
+            prev_1 = kth_begin;
+            kth_begin = kth_begin->next;
+            node = node->next;
+            i++;
+        }
+        
+        while(node){
+            node = node->next;
+            prev_2 = kth_end;
+            kth_end = kth_end->next;
+        }
+        
+        if(kth_begin->next == kth_end){
+            prev_1->next = kth_end;
+            kth_begin->next = kth_end->next;
+            kth_end->next = kth_begin;
+        }
+        else{
+            prev_1->next = kth_end;
+            ListNode *temp = kth_end->next;
+            kth_end->next = kth_begin->next;
+            kth_begin->next = temp;
+            prev_2->next = kth_begin;
+        }
+        
+        return dummy->next;
+    } */
 };
