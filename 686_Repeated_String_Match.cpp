4438d57a410839c2ccec54c3250e59682ca20487
bhushangawde <bhushangawde32@gmail.com>
2022-06-12 12:28:07 +0530

    KMP solution added - fast

--- 686_Repeated_String_Match.cpp
+++ 686_Repeated_String_Match.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     
-	// KMP solution
-    void buildLPS(int lps[], string pat){
+    void buildLPS(vector<int> &lps, string &pat){
         int i = 1;
         int len = 0;
         lps[0] = 0;
@@ -25,42 +24,49 @@ public:
         }
     }
     
+    bool KMP(string &c, string &b, vector<int> &lps){
+        int patLen = b.size();
+        int i = 0;
+        int j = 0;
+        while(i < c.size()){
+            if(c[i] == b[j]){
+                i++;
+                j++;
+            }   
+            if(j == patLen){
+                return true;
+            }
+            else if(i < c.size() && c[i] != b[j]){
+                if(j != 0)
+                    j = lps[j - 1];
+                else
+                    i++;
+            }
+        }
+        return false;
+    }
+    
     int repeatedStringMatch(string a, string b) {
         int patLen = b.size();
         int num = 1;
         string c = a;
-        int lps[patLen];
+        vector<int> lps(patLen, 0);
         buildLPS(lps, b);
         int ctr = 0;
         
-        while(c.size() <= 2*b.size() || ctr < 2){
-            if(c.size() < b.size()){
-                c += a;
-                num++;
-                continue;
-            }
-                
-            int i = 0;
-            int j = 0;
-            while(i < c.size()){
-                if(c[i] == b[j]){
-                    i++;
-                    j++;
-                }   
-                if(j == patLen){
-                    return num;
-                }
-                else if(i < c.size() && c[i] != b[j]){
-                    if(j != 0)
-                        j = lps[j - 1];
-                    else
-                        i++;
-                }
-            }
-            num++;
+        while(c.size() < b.size()){
             c += a;
-            ctr++;
+            num++;
         }
+        
+        if(KMP(c, b, lps))
+            return num;
+        
+        num++;
+        c += a;
+        
+        if(KMP(c, b, lps))
+            return num;
         return -1;
     }
 };
