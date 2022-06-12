6a40b8da3fc0ac87a9033b6ec23002b9cb5bdb75
bhushangawde <bhushangawde32@gmail.com>
2022-06-12 11:18:45 +0530

    New approach

--- 459_Repeated_substring_pattern.cpp
+++ 459_Repeated_substring_pattern.cpp
@@ -31,4 +31,27 @@ public:
         
         return lps[len - 1] && (lps[len - 1] % (len - lps[len - 1]) == 0);
     }
+	
+	
+	// No KMP. Used substring approach.  https://leetcode.com/problems/repeated-substring-pattern/discuss/94346/29-ms-CPP-simple-solution.-No-KMP.
+	
+	/* bool repeatedSubstringPattern(string s) {
+        int len = s.size();
+        
+        if(len == 1)
+            return false;
+        
+        string first, second, s2;
+        for(int i = 1; i <= len/2; i++){
+            if(len % i == 0){
+                first = s.substr(0, i);
+                second = s.substr(i);
+                s2 = second + first;
+                
+                if(s == s2)
+                    return true;
+            }
+        }
+        return false;
+    } */
 };
