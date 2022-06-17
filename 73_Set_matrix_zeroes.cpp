8374b60720a697a09f2d4684e74061799c9e1d26
bhushangawde <bhushangawde32@gmail.com>
2022-06-17 15:59:18 +0530

    O(1) space solution

--- 73_Set_matrix_zeroes.cpp
+++ 73_Set_matrix_zeroes.cpp
@@ -1,5 +1,55 @@
 class Solution {
 public:
+
+	// O(1) space solution
+	void setZeroes(vector<vector<int>>& matrix) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        bool isCol = false;
+        bool isRow = false;
+        
+        for(int i = 0 ; i < rows; i++){
+            if(matrix[i][0] == 0)
+                isCol = true;
+        }
+        
+        for(int j = 0 ; j < cols; j++){
+            if(matrix[0][j] == 0)
+                isRow = true;
+        }
+        
+        for(int i = 1 ; i < rows; i++){
+            for(int j = 1 ; j < cols; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        
+        for(int i = 1 ; i < rows; i++){
+            for(int j = 1 ; j < cols; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+        if(isCol){
+            for(int i = 0 ; i < rows; i++){
+                matrix[i][0] = 0;
+            }
+        }
+        
+        if(isRow){
+            for(int j = 0 ; j < cols; j++){
+                matrix[0][j] = 0;
+            }
+        }
+    }
+	
+
+	// O(n+m) space solution	
     void setZeroes(vector<vector<int>>& matrix) {
         int rows = matrix.size();
         int cols = matrix[0].size();
