ca30b4d26a0c2f68f279932b1947cff05ef32e8e
bhushangawde <bhushangawde32@gmail.com>
2022-06-20 11:41:15 +0530

    Iterative queue implementation

--- 100_Same_tree.cpp
+++ 100_Same_tree.cpp
@@ -11,6 +11,37 @@
  */
 class Solution {
 public:
+
+	// Iteratie solution using queue O(n) space
+	bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(!p && !q)
+            return true;
+        if(!p || !q)
+            return false;
+        
+        queue<TreeNode *> que;
+        que.push(p);
+        que.push(q);
+        
+        while(!que.empty()){
+            TreeNode *first = que.front(); que.pop();
+            TreeNode *second = que.front(); que.pop();
+            
+            if(first == NULL && second == NULL)
+                continue;
+            if(first == NULL || second == NULL)
+                return false;
+            if(first->val != second->val)
+                return false;
+            
+            que.push(first->left);
+            que.push(second->left);
+            que.push(first->right);
+            que.push(second->right);
+        }
+        
+        return true;
+    }
 	
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(!p && !q)
