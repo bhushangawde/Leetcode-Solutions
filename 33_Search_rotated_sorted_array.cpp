a1a4d03caf6c86bb05b29cdf11a478d1c66cc49f
bhushangawde <bhushangawde32@gmail.com>
2022-06-24 12:21:59 +0530

    Clean code

--- 33_Search_rotated_sorted_array.cpp
+++ 33_Search_rotated_sorted_array.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-    int search(vector<int>& arr, int target) {
-        int f = 0;
-        int l = arr.size() - 1;
+    int search(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size() - 1;
+        int mid;
         
-        if(l == -1)
-            return -1;
-        
-        while(f<=l){
-            int mid = f + (l-f)/2;
-            if(arr[mid] == target)
+        while(low <= high){
+            mid = low + (high - low)/2;
+            if(nums[mid] == target)
                 return mid;
             
-            if(arr[mid] >= arr[l]){
-                if(target >= arr[f] && target <= arr[mid])
-                    l = mid-1;
+            if(nums[mid] < nums[low]){
+                if(target > nums[mid] && target <= nums[high])
+                    low = mid + 1;
                 else
-                    f = mid+1;
+                    high = mid - 1;
             }
-            else{
-                if(target <= arr[l] && target >= arr[mid])
-                    f = mid+1;
+            else if (nums[mid] >= nums[low]){
+                if(target < nums[mid] && target >= nums[low])
+                    high = mid - 1;
                 else
-                    l = mid-1;
+                    low = mid + 1;
             }
         }
-        
         return -1;
     }
 };
