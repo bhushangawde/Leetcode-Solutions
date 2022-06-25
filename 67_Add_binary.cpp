a2cd97d582b174bf686cd54d83baaea952d759fd
bhushangawde <bhushangawde32@gmail.com>
2022-06-25 15:36:22 +0530

    Addition using integer

--- 67_Add_binary.cpp
+++ 67_Add_binary.cpp
@@ -1,5 +1,26 @@
 class Solution {
 public:
+	// Addition by first CONVERTING TO INTEGERS  
+	string addBinary(string a, string b) {
+        int aPtr = a.size() - 1;
+        int bPtr = b.size() - 1;
+        string ans = "";
+        int carry = 0;
+        
+        while(aPtr >= 0  || bPtr >= 0){
+            int val1 = aPtr>=0 ? a[aPtr--] - '0' : 0;
+            int val2 = bPtr>=0 ? b[bPtr--] - '0' : 0;
+            int sum = val1 + val2 + carry;
+            carry = sum > 1 ? 1 : 0;
+            ans += to_string(sum % 2);
+        }
+        if(carry == 1)
+            ans += "1";
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+	
+	// Addition using strings 
     string addBinary(string a, string b) {
         string carry = "0";
         int len1 = a.size();
