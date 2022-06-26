ffd9f8e1e257b97b1aa0db626439ee4474a9c40b
bhushangawde <bhushangawde32@gmail.com>
2022-06-26 12:10:04 +0530

    One loop solution

--- 58_length_of_last_word.cpp
+++ 58_length_of_last_word.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+	
+	// One loop solution
+	int lengthOfLastWord(string s) {
+        int len = s.size();
+        int itr = len - 1;
+        int ans = 0;
+        while(itr >=0){
+            if(s[itr] != ' ')
+                ans++;
+            else if(ans > 0)
+                return ans;
+            itr--;
+        }
+        return ans;
+    }
+	
+	// Two loops solution
     int lengthOfLastWord(string s) {
         int len = s.size();
         int itr = len - 1;
