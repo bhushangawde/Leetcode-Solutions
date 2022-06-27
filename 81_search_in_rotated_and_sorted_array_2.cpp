0e0f1cbddd029cdf48f99d48137493d19030fb0f
bhushangawde <bhushangawde32@gmail.com>
2022-06-28 01:08:29 +0530

    Commented cout

--- 81_search_in_rotated_and_sorted_array_2.cpp
+++ 81_search_in_rotated_and_sorted_array_2.cpp
@@ -11,24 +11,24 @@ public:
                 return true;
             
             if(nums[mid] == nums[low] && nums[mid] == nums[high]){
-                cout<<"Condition 1" << endl;
+                // cout<<"Condition 1" << endl;
                 low++;
                 high--;
                 continue;
             }
             if(nums[mid] == nums[low]){
-                cout<<"Condition 2" << endl;
+                // cout<<"Condition 2" << endl;
                 low = mid + 1;
             }
             else if(nums[mid] < nums[low]){
-                cout<<"Condition 3" << endl;
+                // cout<<"Condition 3" << endl;
                 if(target > nums[mid] && target <= nums[high])
                     low = mid + 1;
                 else
                     high = mid - 1;
             }
             else if (nums[mid] > nums[low]){
-                cout<<"Condition 4" << endl;
+                // cout<<"Condition 4" << endl;
                 if(target < nums[mid] && target >= nums[low])
                     high = mid - 1;
                 else
