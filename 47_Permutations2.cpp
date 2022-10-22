be41e67aacbc7c4e4714b9173de8d8c03c4fc859
Bhushan Gawde <bhushangawde32@gmail.com>
2022-10-22 16:49:45 +0530

    Permutation 2 using set to store all combinations

---
+++ 47_Permutations2.cpp
@@ -0,0 +1,29 @@
+// Slight changes over permutation 1
+class Solution {
+public:
+    void solve(vector<vector<int>> &ans, vector<int> &nums, int n){
+        if(n == nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i = 0 ; i <= n ; i++){
+            swap(nums[i], nums[n]);
+            solve(ans,nums,n+1);
+            swap(nums[i], nums[n]);
+        }
+    }
+    
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n = nums.size();
+        solve(ans, nums, 0);
+        set<vector<int>> st;
+        for(auto it : ans)
+            st.insert(it);
+        
+        vector<vector<int>> ans_2(st.begin(), st.end());
+        
+        return ans_2;
+    }
+};
+
