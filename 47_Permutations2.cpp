719689295b933eef07fd8fd669b5474c3fcb60ad
Bhushan Gawde <bhushangawde32@gmail.com>
2022-10-22 16:50:15 +0530

    Permutation 2 using set to prune

--- 47_Permutations2.cpp
+++ 47_Permutations2.cpp
@@ -1,3 +1,36 @@
+// Fast solution
+class Solution {
+public:
+    void solve(vector<vector<int>> &ans, vector<int> &nums, int n){
+        if(n == nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        unordered_set<int> st;
+        
+        for(int i = n ; i < nums.size() ; i++){
+            
+            if(st.find(nums[i]) != st.end()){
+                continue;
+            }
+            st.insert(nums[i]);
+            
+            swap(nums[i], nums[n]);
+            solve(ans,nums,n+1);
+            swap(nums[i], nums[n]);
+        }
+    }
+    
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n = nums.size();
+        solve(ans, nums, 0);
+        return ans;
+    }
+};
+
+
+
 // Slight changes over permutation 1
 class Solution {
 public:
