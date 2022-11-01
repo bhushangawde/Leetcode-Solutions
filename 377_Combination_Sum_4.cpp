eb9e6979ec39ffc8e9d5c0c1bc4d0c1abd1df6bb
Bhushan Gawde <bhushangawde32@gmail.com>
2022-11-02 00:02:48 +0530

    bottom up dp

--- 377_Combination_Sum_4.cpp
+++ 377_Combination_Sum_4.cpp
@@ -48,3 +48,23 @@ public:
     }
 };
 
+// Tabulation
+class Solution {
+public:
+	int solveTab(vector<int>& nums, int target, vector<unsigned int>&dp){
+        dp[0] = 1;
+        for(int i = 1; i <= target; i++){
+            for(int j = 0; j < nums.size(); j++){
+                if(nums[j] <= i)
+                    dp[i] += dp[i - nums[j]];
+            }
+        }
+        return dp[target];
+    }
+
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<unsigned int> dp(target + 1, 0);
+        // return solveMemo(nums, target, dp);
+        return solveTab(nums, target, dp);
+    }
+};
