bfc0e5cc9357449691c4445ecdfe99a3f3c68ef5
Bhushan Gawde <bhushangawde32@gmail.com>
2022-11-02 00:02:34 +0530

    naive recursion + memoization dp

--- 377_Combination_Sum_4.cpp
+++ 377_Combination_Sum_4.cpp
@@ -21,3 +21,30 @@ public:
     }
 };
 
+
+// Memoization
+class Solution {
+public:
+
+    int solveMemo(vector<int>& nums, int target, vector<int>&dp){
+        if(target < 0)
+            return 0;
+        if(target == 0)
+            return 1;
+
+        if(dp[target] != -1)
+            return dp[target];
+
+        long ans = 0;
+        for(int i = 0; i < nums.size(); i++){
+            ans += solveMemo(nums, target - nums[i], dp);
+        }
+        return dp[target] = ans;
+    }
+
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target + 1, -1);
+        return solveMemo(nums, target, dp);
+    }
+};
+
