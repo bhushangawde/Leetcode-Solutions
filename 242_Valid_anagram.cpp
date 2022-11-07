f1566ec96099fcc00656d8454e5f280466da4b7e
Bhushan Gawde <bhushangawde32@gmail.com>
2022-11-07 20:00:47 +0530

    O(n) for all unicode chars

--- 242_Valid_anagram.cpp
+++ 242_Valid_anagram.cpp
@@ -1,3 +1,25 @@
+// Unordered map (Works for all unicode characters)
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size())
+            return false;
+
+        int len = s.size();
+        unordered_map<char, int> ump;
+        for(int i = 0; i < len; i++){
+            ump[s[i]]++;
+            ump[t[i]]--;
+        }
+
+        for(auto elem : ump){
+            if(elem.second != 0)
+                return false;
+        }
+        return true; 
+    }
+};
+
 // Sorting solution
 class Solution {
 public:
