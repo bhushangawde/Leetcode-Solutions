ef24e04a64f76423da30ff781e3584aacc0b7392
Bhushan Gawde <bhushangawde32@gmail.com>
2022-11-14 12:00:51 +0530

    division operator

--- 231_Power_of_2.cpp
+++ 231_Power_of_2.cpp
@@ -1,3 +1,20 @@
+// Using bit count
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n <= 0)
+            return false;
+        int cnt = 0;
+        while(n > 0){
+            if(n & 1 == 1){
+                cnt++;
+            }
+            n = n >> 1;
+        }
+        return cnt == 1;
+    }
+};
+
 // Using division operator
 class Solution {
 public:
