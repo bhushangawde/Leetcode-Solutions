5567634623cf7f158afd23365b756f2219e8bd50
Bhushan Gawde <bhushangawde32@gmail.com>
2022-11-15 10:10:16 +0530

    Brute force

---
+++ 258_Add_digits.cpp
@@ -0,0 +1,16 @@
+// Brute force solution
+class Solution {
+public:
+    int addDigits(int num) {
+        int sum;
+        while(num>9){
+            sum = 0;
+            while(num){
+                sum += num % 10;
+                num = num/10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+};
