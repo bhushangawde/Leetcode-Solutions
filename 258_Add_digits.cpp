0cae20d4ae5a8304ee6d87fa5aeebc61d1befdc3
Bhushan Gawde <bhushangawde32@gmail.com>
2022-11-15 10:12:37 +0530

    observation

--- 258_Add_digits.cpp
+++ 258_Add_digits.cpp
@@ -1,3 +1,15 @@
+// Divisibility by 9
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num<=9)
+            return num;
+        if(num%9 == 0)
+            return 9;
+        return num%9;
+    }
+};
+
 // Brute force solution
 class Solution {
 public:
