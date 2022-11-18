7ab0db5febd6a78c96f832778782468f7a9389e1
Bhushan Gawde <bhushangawde32@gmail.com>
2022-11-18 11:20:22 +0530

    recurrsive

---
+++ 129_Sum_Root_to_leaf_Numbers.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void solve(TreeNode* node, int &sum, int &currSum){
+        if(!node){
+            return;
+        }
+        currSum = currSum*10 + node->val;
+        if(!node->left && !node->right){
+            sum += currSum;
+            currSum = (currSum - node->val)/10;
+            return;
+        }
+        
+        solve(node->left, sum, currSum);
+        solve(node->right, sum, currSum);
+        currSum = (currSum - node->val)/10;
+    }
+
+    int sumNumbers(TreeNode* root) {
+        int currSum = 0;
+        int sum = 0;
+        solve(root, sum, currSum);
+        return sum;
+    }
+};
