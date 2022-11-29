a90ebb0d73143666f5c52f4ee8297d8e6dd98d66
Bhushan Gawde <bhushangawde32@gmail.com>
2022-11-29 11:07:25 +0530

    Using max heap

--- 973_k_closest_points_to_origin.cpp
+++ 973_k_closest_points_to_origin.cpp
@@ -1,3 +1,29 @@
+// Using Priority Queue max heapcheck
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        vector<vector<int>> ans;
+        priority_queue<pair<int, pair<int,int>>> pq;
+
+        for(int i = 0 ; i < points.size(); i++){
+            int x = points[i][0];
+            int y = points[i][1];
+            pq.push({x*x + y*y, {x, y}});
+
+            if(pq.size() > k){
+                pq.pop();
+            }
+        }
+
+        while(pq.size() > 0){
+            ans.push_back({pq.top().second.first, pq.top().second.second});
+            pq.pop();
+        }
+
+        return ans;
+    }
+};  
+
 class Solution {
 public:
     
