536c737d073afad48085e23637596618843dfa97
Bhushan Gawde <bhushangawde32@gmail.com>
2022-11-30 15:19:54 +0530

    Quick select. O(n) average

--- 347_top_k_frequent_elements.cpp
+++ 347_top_k_frequent_elements.cpp
@@ -1,3 +1,61 @@
+
+// QuickSelect
+class Solution {
+public:
+
+    void swap(int &a, int &b){
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    int partition(vector<int> &arr, int low, int high, unordered_map<int, int> &ump){
+        int i = low;
+        int pivot = arr[high];
+
+        for(int j = low; j < high; j++){
+            if(ump[arr[j]] >= ump[pivot]){
+                swap(arr[i], arr[j]);
+                i++;
+            }
+        }
+        swap(arr[i], arr[high]);
+        return i;
+    }
+
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> ump;
+        vector<int> ans(k);
+        for(auto elem : nums)
+            ump[elem]++;
+        
+        vector<int> arr;
+        for(auto elem : ump){
+            // cout << elem.first << " "<<elem.second << endl;
+            arr.push_back(elem.first);
+        }
+
+        int low = 0;
+        int high = arr.size() - 1;
+
+        while(1){
+            int pivotIdx = partition(arr, low, high, ump);
+            if(pivotIdx == k-1){
+                // for(int i = 0 ; i < k; i++)
+                //     ans.push_back(arr[i]);
+                copy_n(arr.begin(), k, ans.begin());
+                return ans;
+            }
+
+            if(pivotIdx < k - 1)
+                low = pivotIdx + 1;
+            else 
+                high = pivotIdx - 1;
+        }
+  return {};
+    }
+};
+
 // Priority queue
 class Solution {
 public:
