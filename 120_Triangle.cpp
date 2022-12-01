fad586f61bdb671dcfe2c24dd646b7f879c2a58d
Bhushan Gawde <bhushangawde32@gmail.com>
2022-12-01 11:52:04 +0530

    DP O(n) space optimized

--- 120_Triangle.cpp
+++ 120_Triangle.cpp
@@ -1,3 +1,28 @@
+// O(n) space solution
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        vector<int> prev(201, INT_MAX);
+        vector<int> curr(201, INT_MAX);
+        
+        int size = triangle.size();
+        prev[1] = triangle[0][0];
+        
+        for(int i = 2; i <= size; i++){
+            for(int j = 1; j <= i; j++){
+                curr[j] = triangle[i - 1][j - 1] + min(prev[j - 1], prev[j]);
+            }
+            prev = curr;
+        }
+       
+        int min_ = INT_MAX;
+        for(int i = 1; i <= size; i++){
+            min_ = min(min_, prev[i]);
+        }
+        return min_;
+    }
+};
+
 // O(n2) space solution
 class Solution {
 public:
