c6f69db780b0c1ec3e5fe530e7c84ca7856837be
Bhushan Gawde <bhushangawde32@gmail.com>
2022-12-14 11:39:59 +0530

    DP - O(N) Space bottom-up

--- 198_House_Robber.cpp
+++ 198_House_Robber.cpp
@@ -1,4 +1,19 @@
 
+// Bottom - Up DP
+class Solution {
+public:
+
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size() + 1, -1);
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for(int i = 2; i <= nums.size(); i++){
+            dp[i] = max(nums[i-1] + dp[i-2], dp[i-1]);
+        }
+        return dp[nums.size()];
+    }
+};
+
 // Recursion + memoization
 class Solution {
 public:
