fd53952173dd8408e3f94b3d0f4f8e0c49346e7e
Bhushan Gawde <bhushangawde32@gmail.com>
2022-12-14 11:39:40 +0530

    Top-down dp

--- 198_House_Robber.cpp
+++ 198_House_Robber.cpp
@@ -1,4 +1,24 @@
 
+// Recursion + memoization
+class Solution {
+public:
+
+    int solve(vector<int>& nums, vector<int>& dp, int idx){
+        if(idx >= nums.size())
+            return 0;        
+        if(dp[idx] != -1) 
+            return dp[idx];
+        dp[idx] = max(nums[idx] + solve(nums, dp, idx + 2), solve(nums, dp, idx + 1));
+        return dp[idx];
+    }
+
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size(), -1);
+        return solve(nums, dp, 0);
+        return dp[nums.size()-1];
+    }
+};
+
 // Plain Recursion
 class Solution {
 public:
