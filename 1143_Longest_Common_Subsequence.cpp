7a1e72e254c3205b1ea82ad4cbcc93159117da47
Bhushan Gawde <bhushangawde32@gmail.com>
2022-12-15 12:19:02 +0530

    DP - O(n2) extra Space bottom-up

--- 1143_Longest_Common_Subsequence.cpp
+++ 1143_Longest_Common_Subsequence.cpp
@@ -1,3 +1,32 @@
+// O(n) space Bottom-Up DP
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int size1 = text1.size();
+        int size2 = text2.size();
+        
+        vector<int> curr(size2+1, 0);
+        vector<int> prev(size2+1, 0);
+        
+        for(int j = 0; j <= size2; j++)
+            prev[j] = 0;
+
+        for(int i = 1; i <= size1; i++){
+            curr[0] = 0;
+            for(int j = 1; j <= size2; j++){
+                if(text1[i-1] == text2[j-1]){
+                    curr[j] = 1 + prev[j-1];
+                }
+                else{
+                    curr[j] = max(prev[j], curr[j-1]);
+                }
+            }
+            prev = curr;
+        }
+        
+        return prev[size2];
+    }
+};
 
 // O(n2) space Bottom-Up DP
 class Solution {
