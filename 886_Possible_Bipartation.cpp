9124e1f662ff818697130aa91c39190bfccabbc3
Bhushan Gawde <bhushangawde32@gmail.com>
2022-12-21 23:15:57 +0530

    DFS

--- 886_Possible_Bipartation.cpp
+++ 886_Possible_Bipartation.cpp
@@ -35,3 +35,41 @@ public:
         return true;
     }
 };
+
+// DFS
+class Solution {
+public:
+    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
+        unordered_map<int, vector<int>> adj;
+        vector<bool> vis(n + 1, false);
+        vector<int> color(n + 1, 0);
+        for(int i = 0; i < dislikes.size(); i++){
+            adj[dislikes[i][0]].push_back(dislikes[i][1]);
+            adj[dislikes[i][1]].push_back(dislikes[i][0]);
+        }
+        stack<int> dfs;
+        for(int i = 1 ; i <= n; i++){
+            if(!vis[i]){
+                dfs.push(i);
+                color[i] = 1;
+                while(!dfs.empty()){
+                    int node = dfs.top();
+                    dfs.pop();
+                    for(auto elem : adj[node]){
+                        if(!vis[elem]){
+                            vis[elem] = true;
+                            color[elem] = color[node]*(-1);
+                            dfs.push(elem);
+                        }
+                        else{
+                            if(color[elem] == color[node]){
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
