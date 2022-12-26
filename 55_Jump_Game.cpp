2753df4263da18315538846533ad3fa123efbfb7
Bhushan Gawde <bhushangawde32@gmail.com>
2022-12-26 17:31:14 +0530

    ADDED MEMOIZATION

--- 55_Jump_Game.cpp
+++ 55_Jump_Game.cpp
@@ -1,4 +1,28 @@
 
+// Recursion + memoization
+class Solution {
+public:
+    bool solve(int idx, vector<int>& nums, vector<int> &dp){
+        if(idx == nums.size() - 1)
+            return true;
+        if(nums[idx] == 0)
+            return dp[idx] = false;
+        if(dp[idx] != -1)
+            return dp[idx];
+
+        for(int k = 1; k <= nums[idx]; k++){
+            if(solve(idx + k, nums, dp)){
+                return dp[idx] = true;
+            }
+        }
+        return dp[idx] = false;
+    }
+
+	bool canJump(vector<int>& nums) {
+        vector<int> dp(nums.size() + 1, -1);
+        return solve(0, nums, dp);
+    }
+};
 
 // Recursion
 class Solution {
