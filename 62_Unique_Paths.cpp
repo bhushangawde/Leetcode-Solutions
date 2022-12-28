b86f02f2a56186df8f357ca598998aaf209cdc6d
Bhushan Gawde <bhushangawde32@gmail.com>
2022-12-28 19:52:41 +0100

    Bottom Up tabulation optimized

--- 62_Unique_Paths.cpp
+++ 62_Unique_Paths.cpp
@@ -1,3 +1,22 @@
+// Bottom-Up DP - O(n) space
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> prev(n, -1);
+        vector<int> curr(n, -1);
+        for(int i = 0 ; i < n; i++)
+            prev[i] = 1;
+
+        for(int i = 1; i < m; i++){
+            curr[0] = 1;
+            for(int j = 1; j < n; j++){
+                curr[j] = curr[j-1] + prev[j];
+            }
+            prev = curr;
+        }
+        return prev[n-1];
+    }
+};
 
 // Bottom-Up DP - O(n2) space
 class Solution {
