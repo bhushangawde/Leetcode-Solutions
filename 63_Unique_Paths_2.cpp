e9a586f60a8985ad0c97f030ca03164750744159
Bhushan Gawde <bhushangawde32@gmail.com>
2022-12-29 00:24:22 +0530

    Bottom Up tabulation DP

--- 63_Unique_Paths_2.cpp
+++ 63_Unique_Paths_2.cpp
@@ -1,3 +1,41 @@
+// Bottom-up DP
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        if(obstacleGrid[0][0])
+            return 0;
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<long>> dp(m, vector<long>(n, 0));
+        for(int i = 0 ; i < m; i++){
+            if(obstacleGrid[i][0]){
+                dp[i][0] = 0;
+                break;
+            }
+            else 
+                dp[i][0] = 1;
+        }
+        for(int i = 0 ; i < n; i++){
+            if(obstacleGrid[0][i]){
+                dp[0][i] = 0;
+                break;
+            }
+            else
+                dp[0][i] = 1;
+        }
+
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(obstacleGrid[i][j])
+                    dp[i][j] = 0;
+                else 
+                    dp[i][j] = dp[i][j-1] + dp[i-1][j];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
 
 // Plain recursion
 class Solution {
