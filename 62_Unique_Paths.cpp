25d04352820b3756e03a9eb712166df4085d69a6
Bhushan Gawde <bhushangawde32@gmail.com>
2022-12-28 19:52:03 +0100

    Top down memoization 2

--- 62_Unique_Paths.cpp
+++ 62_Unique_Paths.cpp
@@ -1,5 +1,21 @@
 
 
+// Top Down DP - Memoization - (0,0) -> (m,n)
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n));
+        return dfs(dp, 0, 0);
+    }
+    int dfs(vector<vector<int>>& dp, int i, int j) {
+        if(i >= size(dp)   || j >= size(dp[0]))   return 0;  
+        if(i == size(dp)-1 && j == size(dp[0])-1) return 1;    
+        if(dp[i][j]) return dp[i][j];                           
+        return dp[i][j] = dfs(dp, i+1, j) + dfs(dp, i, j+1);    
+    }
+};
+
+
 // Top Down DP - Memoization - (m,n) -> (0,0)
 class Solution {
 public:
