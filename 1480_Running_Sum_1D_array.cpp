93a44b5d7a46f91ea6f5c8918f79d171fdfcbec3
Bhushan Gawde <bhushangawde32@gmail.com>
2023-01-05 13:02:50 +0530

    Input not modified

--- 1480_Running_Sum_1D_array.cpp
+++ 1480_Running_Sum_1D_array.cpp
@@ -1,3 +1,18 @@
+
+// Not modifying input
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> ans(nums.size(), 0);
+        ans[0] = nums[0];
+        for(int i = 1 ; i < nums.size(); i++){
+            ans[i] = nums[i] + ans[i-1];
+        }
+        return ans;
+    }
+};
+
+// Modifies input
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
