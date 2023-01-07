f0393aaeaf2e3534c2ed55469aef152c8de80f4c
Bhushan Gawde <bhushangawde32@gmail.com>
2023-01-07 16:13:22 +0530

    Brian Kernighan's Algorithm

--- 461_Hamming_distance.cpp
+++ 461_Hamming_distance.cpp
@@ -1,5 +1,4 @@
 // XOR
-
 class Solution {
 public:
     int hammingDistance(int x, int y) {
@@ -8,6 +7,20 @@ public:
         while(x_>0){
             bits += x_ & 1;
             x_ >>=1;
+        }
+        return bits;
+    }
+};
+
+// Brian Kernighan's Algorithm for XOR 
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int x_ = x^y;
+        int bits = 0;
+        while(x_ > 0){
+            bits += 1;
+            x_ = x_ & (x_ - 1) ;
         }
         return bits;
     }
