d2783fb2728ae2493036123cd3d97c8580e8a89b
Bhushan Gawde <bhushangawde32@gmail.com>
2023-01-07 17:37:45 +0530

    Two freq. maps

--- 350_Intersection_of_2arrays_2.cpp
+++ 350_Intersection_of_2arrays_2.cpp
@@ -1,3 +1,5 @@
+
+// Using 2 maps
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
@@ -20,41 +22,4 @@ public:
         
         return ans;
     }
-	
-	
-	// Another Solution
-/* 	vector<int> intersect(vector<int>& a, vector<int>& b) {
-		unordered_map<int, int> ctr;
-		for (int i : a)
-			ctr[i]++;
-		vector<int> out;
-		for (int i : b)
-			if (ctr[i]-- > 0)
-				out.push_back(i);
-		return out;
- 	}
-	*/
-	
-	// Yet another solution
-	
-	/* vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-       sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
-        vector<int> v;
-        int i=0,j=0;
-        while(i<nums1.size() && j<nums2.size()){
-            if(nums1[i]<nums2[j]){
-                i++;
-            }else if(nums1[i]>nums2[j]){
-                j++;
-            }else{
-                v.push_back(nums1[i]);
-                i++;j++;
-            }
-        }
-        return v;
-    } */
-	
-	
-	
 };
