575f1b5843a739aee034b96de6c632e6729e69dc
Bhushan Gawde <bhushangawde32@gmail.com>
2023-01-08 20:21:04 +0530

    BFS

--- 210_course_Schedule_2.cpp
+++ 210_course_Schedule_2.cpp
@@ -1,3 +1,40 @@
+// BFS solution
+class Solution {
+public:
+
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> adjList[numCourses];
+        vector<int> indegree(numCourses, 0);
+        queue<int> q;
+        vector<int> ans;
+        for(int i = 0; i < prerequisites.size(); i++){
+            adjList[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            indegree[prerequisites[i][0]]++;
+        }
+
+        for(int i = 0 ; i < numCourses; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        int cnt = 0;
+        while(!q.empty()){
+            int v = q.front();
+            q.pop();
+            cnt++;
+            ans.push_back(v);
+
+            for(auto node : adjList[v]){
+                if(--indegree[node] == 0){
+                    q.push(node);
+                }
+            }    
+        }
+        if(cnt == numCourses)
+            return ans;
+        return {};
+    }
+};
 
 // DFS solution
 class Solution {
