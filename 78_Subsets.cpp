3c0154883a2a2424ebf1825ad276ca86e678bacf
Bhushan Gawde <bhushangawde32@gmail.com>
2023-01-10 04:19:48 +0530

    ANother solution

--- 78_Subsets.cpp
+++ 78_Subsets.cpp
@@ -1,3 +1,23 @@
+// Solution 1
+class Solution {
+public:
+    vector<vector<int>> result;
+    
+    void powerset(vector<int> nums, int curr, vector<int> res, int n){
+        result.push_back(res);
+        for(int i = curr; i < n; i++){
+            res.push_back(nums[i]);
+            powerset(nums, i+1, res, n);
+            res.pop_back();
+        }
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res;
+        powerset(nums, 0, res, n);
+        return result;
+    }
+};
 
 
 // Another solution
