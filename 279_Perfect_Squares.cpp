18e7d78b450e71d934fc338378e033169143ddd4
Bhushan Gawde <bhushangawde32@gmail.com>
2023-01-20 03:26:20 +0530

    BFS solution

--- 279_Perfect_Squares.cpp
+++ 279_Perfect_Squares.cpp
@@ -1,3 +1,5 @@
+
+
 // DP solution
 class Solution {
 public:
@@ -17,5 +19,49 @@ public:
         }
         
         return dp[n];
+    }
+};
+
+
+// BFS
+class Solution {
+public:
+
+    int numSquares(int n) {
+        vector<int> squares;
+        vector<int> dp(n + 1, INT_MAX);
+
+        dp[0] = 0;
+        int currCnt = 1;
+        queue<int> qs;
+        for(int i = 1; i*i <= n; i++){
+            squares.push_back(i*i);
+            dp[i*i] = 1;
+            qs.push(i*i);
+        }
+
+        if(n == qs.back())
+            return 1;
+            
+        while(!qs.empty()){
+            currCnt++;
+            int size = qs.size();
+            for(int s = 0; s < size; s++){
+                int temp = qs.front();
+                for(int i = 0 ; i < squares.size(); i++){
+                    int val = temp + squares[i];
+                    if(val > n)
+                        break;
+                    else if(val == n)
+                        return currCnt;
+                    else if(val < n && dp[val] == INT_MAX){
+                        dp[val] = currCnt;
+                        qs.push(val);
+                    }
+                }
+                qs.pop();
+            } 
+        }
+        return 0;
     }
 };
