6bd909c5ba153bcdaef4bf18153c6f309656a05f
Bhushan Gawde <bhushangawde32@gmail.com>
2023-01-19 23:12:23 +0100

    Hare and tortoise cycle finding floyd algo

--- 287_Duplicate_Number.cpp
+++ 287_Duplicate_Number.cpp
@@ -1,4 +1,22 @@
-
+// Hare and tortoise
+class Solution {
+public:
+    
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        fast = 0;
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+};
 
 // O(nlogn) time, O(1) space. Binary Search
 class Solution {
