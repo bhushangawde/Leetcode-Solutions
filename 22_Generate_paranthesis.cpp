194a53783ff91a612fab34ffe1407af66bf0a58d
Bhushan Gawde <bhushangawde32@gmail.com>
2023-03-10 16:50:05 +0530

    shorter and optmized solution

--- 22_Generate_paranthesis.cpp
+++ 22_Generate_paranthesis.cpp
@@ -1,3 +1,34 @@
+// Shorter solution
+class Solution {
+public:
+
+    void solve(int n1, int n2, string s, vector<string> &ans){
+        if(n1 == 0 && n2 == 0){
+            ans.push_back(s);
+            return;
+        }
+        
+        if(n1 > 0){
+            string s1 = s;
+            s1.push_back('(');
+            solve(n1-1, n2, s1, ans);
+        }
+        if(n1 < n2){
+            string s2 = s;
+            s2.push_back(')');
+            solve(n1, n2-1, s2, ans);
+        }
+        
+        return;
+    }
+
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string s;
+        solve(n, n, s, ans);
+        return ans;    
+    }
+};
 
 // From recursion tree
 class Solution {
