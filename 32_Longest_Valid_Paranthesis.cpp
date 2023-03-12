7b3784d5c63ac8bcdc4a31a200419608bc81378f
Bhushan Gawde <bhushangawde32@gmail.com>
2023-03-12 15:45:49 +0530

    Stack O(n)

--- 32_Longest_Valid_Paranthesis.cpp
+++ 32_Longest_Valid_Paranthesis.cpp
@@ -1,3 +1,28 @@
+// Using stack
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        if(s.size() <= 1)
+            return 0;
+        stack<int> st;
+        int ans = 0;
+        st.push(-1);
+        for(int i = 0 ; i < s.size(); i++){
+            if(s[i] == '(')
+                st.push(i);
+            else{
+                st.pop();
+                if(st.empty()){
+                    st.push(i);
+                }
+                else{
+                    ans = max(ans, i - st.top());
+                }
+            }
+        }
+        return ans;
+    }
+};
 
 // Using two traversals
 class Solution {
