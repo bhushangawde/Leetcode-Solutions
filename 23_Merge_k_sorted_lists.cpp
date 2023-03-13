60470690b27396652b53f322721d69000b4722d6
Bhushan Gawde <bhushangawde32@gmail.com>
2023-03-14 04:01:05 +0530

    Using heap - O(N)logK

--- 23_Merge_k_sorted_lists.cpp
+++ 23_Merge_k_sorted_lists.cpp
@@ -1,3 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+ 
+ 
+ // USING A PRIORITY QUEUE - STL
+class Solution {
+public:
+    struct compare{
+        bool operator()(ListNode *first, ListNode *second){
+            return first->val > second->val;
+        }
+    };
+    
+    
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        ListNode* dummy = new ListNode(-10001, NULL);
+        priority_queue<ListNode*, vector<ListNode*>, compare> pq;
+        ListNode* newHead = NULL;
+        ListNode* currPtr = dummy;
+        
+        for(int i = 0 ; i < lists.size(); i++){
+            if(lists[i])
+                pq.push(lists[i]);
+        }
+        
+        while(!pq.empty()){
+            ListNode* currMin = pq.top();
+            pq.pop();
+            currPtr->next = currMin;
+            currPtr = currMin;
+            ListNode *next = currMin->next; 
+            if(next){
+                pq.push(next);
+            }
+        }
+        
+        return dummy->next;
+    }
+};
 
 // USING MERGE TWO LISTS AT A TIME - CHANGES THE INPUT LISTS
 /* class Solution {
