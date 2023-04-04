6b98e4b60cf5fd53f58edff76acc39106d7df921
Bhushan Gawde <bhushangawde32@gmail.com>
2023-04-05 03:19:19 +0530

    DP Bottom up optimized O(n)

--- 718_Maximum_Length_of_repeated_subarray.cpp
+++ 718_Maximum_Length_of_repeated_subarray.cpp
@@ -1,3 +1,26 @@
+// Bottom Up O(n)
+class Solution {
+public:
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        vector<int> prev(n2 + 1, 0);
+        vector<int> curr(n2 + 1, 0);
+        int maxLen = 0;
+        for(int i = 1; i <= n1; i++){
+            for(int j = 1; j <= n2; j++){
+                if(nums1[i-1] == nums2[j-1]){
+                    curr[j] = 1 + prev[j-1];
+                    maxLen = max(maxLen, curr[j]);
+                }
+                else 
+                    curr[j] = 0;
+            }
+            prev = curr;
+        }
+        return maxLen;
+    }
+};
 
 
 // Bottom Up O(n2)
