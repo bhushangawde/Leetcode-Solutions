890878318f615925111c6e181531d4d3287b48e0
Bhushan Gawde <bhushangawde32@gmail.com>
2023-07-01 18:07:31 -0400

    Using pruning

--- 2305_Fair_Distribution_of_Cookies.cpp
+++ 2305_Fair_Distribution_of_Cookies.cpp
@@ -1,3 +1,35 @@
+class Solution {
+public:
+    
+    int solve(vector<int>& cookies, int k, int id, vector<int>& freq, int remainingCount){
+        if(cookies.size() - id < remainingCount){
+            return INT_MAX;
+        }
+
+        if(id == cookies.size()){
+            return *max_element(freq.begin(), freq.end());
+        }
+
+        int currFreq = INT_MAX;
+        for(int i = 0; i < k; i++){
+            if(freq[i] == 0)
+                remainingCount--;
+            freq[i] += cookies[id];
+            int tempFreq = solve(cookies, k, id + 1, freq, remainingCount);
+            freq[i] -= cookies[id];
+            if(freq[i] == 0)
+                remainingCount++;
+            currFreq = min(currFreq, tempFreq);
+        }
+
+        return currFreq;
+    }
+
+    int distributeCookies(vector<int>& cookies, int k) {
+        vector<int> freq(k, 0);
+        return solve(cookies, k, 0, freq, k);
+    }
+};
 
 
 // Recursion + Backtracking
