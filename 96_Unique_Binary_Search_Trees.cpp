b7b9e71ff85c967c85e7bfe494bd1d06f9f28470
Bhushan Gawde <bhushangawde32@gmail.com>
2023-07-23 20:32:15 +0530

    Bottom up dp table

--- 96_Unique_Binary_Search_Trees.cpp
+++ 96_Unique_Binary_Search_Trees.cpp
@@ -1,3 +1,25 @@
+// Bottom up DP
+class Solution {
+public:
+    int solve(int n, vector<int>& dp){
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+
+    int numTrees(int n) {
+        vector<int> dp(n + 1, 0);
+        return solve(n, dp);    
+    }
+};
+
+
 // Top down dp - Recursion + memoization
 class Solution {
 public:
