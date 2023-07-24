0748a72642a25ee741160d11c848c28f598992f4
Bhushan Gawde <bhushangawde32@gmail.com>
2023-07-24 22:11:43 +0530

    Recursion + memoization

--- 95_Unique_Binary_Search_Trees_II.cpp
+++ 95_Unique_Binary_Search_Trees_II.cpp
@@ -1,3 +1,51 @@
+// Recursion + Memoization 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    vector<TreeNode*> solve(int start, int end, map<pair<int, int>, vector<TreeNode*>>& dp){
+        if(start > end){
+            return {NULL};
+        }
+
+        if(dp.find({start, end}) != dp.end())
+            return dp[{start, end}];
+
+        vector<TreeNode*> ans;
+
+        for(int node = start; node <= end; node++){
+            vector<TreeNode*> left = solve(start, node - 1, dp);
+            vector<TreeNode*> right = solve(node + 1, end, dp);
+
+            for(auto l : left){
+                for(auto r : right){
+                    TreeNode* root = new TreeNode(node);
+                    root->left = l;
+                    root->right = r;
+                    ans.push_back(root);
+                }
+            }
+        }
+
+        return dp[{start, end}] = ans;
+
+    }
+    
+    vector<TreeNode*> generateTrees(int n) {
+        map<pair<int, int>, vector<TreeNode*>> dp;
+        return solve(1, n, dp);
+    }
+};
 
 
 // Recursion
