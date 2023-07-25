d785a349b1eb75690b0a4b71720e6f382928b0c3
Bhushan Gawde <bhushangawde32@gmail.com>
2023-07-26 02:30:04 +0530

    Binary search simplified

--- 852_Peak_Index_in_a_Mountain_Array.cpp
+++ 852_Peak_Index_in_a_Mountain_Array.cpp
@@ -1,3 +1,27 @@
+// Simplified Binary Search 
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int start = 0;
+        int end = arr.size() - 1;
+
+        while(start < end){
+            int mid = start + (end - start) / 2;
+
+            if(arr[mid] < arr[mid + 1]){
+                start = mid + 1;
+            }
+            else {
+                end = mid;
+            }
+        }
+
+        return end;
+    }
+};
+
+
+// Binary Search 
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
