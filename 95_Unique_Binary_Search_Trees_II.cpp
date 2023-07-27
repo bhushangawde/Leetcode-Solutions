eb355e17a93e89d1bc98cdfe83c22e644d89d7d0
Bhushan Gawde <bhushangawde32@gmail.com>
2023-07-27 12:24:01 +0530

    Bottom up dp

--- 95_Unique_Binary_Search_Trees_II.cpp
+++ 95_Unique_Binary_Search_Trees_II.cpp
@@ -1,3 +1,90 @@
+// Bottom up dp
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    vector<TreeNode*> solve(int start, int end, map<pair<int, int>, vector<TreeNode*>>& dp){
+        if(start > end){
+            return {NULL};
+        }
+
+        if(dp.find({start, end}) != dp.end())
+            return dp[{start, end}];
+
+        vector<TreeNode*> ans;
+
+        for(int node = start; node <= end; node++){
+            vector<TreeNode*> left = solve(start, node - 1, dp);
+            vector<TreeNode*> right = solve(node + 1, end, dp);
+
+            for(auto l : left){
+                for(auto r : right){
+                    TreeNode* root = new TreeNode(node);
+                    root->left = l;
+                    root->right = r;
+                    ans.push_back(root);
+                }
+            }
+        }
+
+        return dp[{start, end}] = ans;
+
+    }
+    
+    vector<TreeNode*> generateTrees(int n) {
+        vector<vector<vector<TreeNode*>>> dp(n + 1, vector(n + 1, vector<TreeNode*>(0)));
+        
+        for(int i = 1; i <= n; i++){
+            dp[i][i] = {new TreeNode(i)};
+        }
+
+        for(int numNodes = 2; numNodes <= n; numNodes++){
+            for(int start = 1; start <= n - numNodes + 1; start++){
+                int end = start + numNodes - 1;
+                for(int node = start; node <= end; node++){
+                    vector<TreeNode*> left;
+                    vector<TreeNode*> right;
+                    if(node - 1 < start){
+                        left = {NULL};
+                    }
+                    else {
+                        left = dp[start][node - 1];
+                    }
+
+                    if(node + 1 > end){
+                        right = {NULL};
+                    }
+                    else {
+                        right = dp[node + 1][end];
+                    }
+
+                    for(auto l : left){
+                        for(auto r : right){
+                            TreeNode* root = new TreeNode(node);
+                            root->left = l;
+                            root->right = r;
+                            dp[start][end].push_back(root);
+                        }
+                    }
+                }
+            }
+        }
+        return dp[1][n];
+    }
+};
+
+
+
 // Recursion + Memoization 
 /**
  * Definition for a binary tree node.
