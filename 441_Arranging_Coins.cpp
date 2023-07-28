ac54e8253b3082d19ebc392d230834d43434b280
Bhushan Gawde <bhushangawde32@gmail.com>
2023-07-28 22:28:57 +0530

    Binary search

--- 441_Arranging_Coins.cpp
+++ 441_Arranging_Coins.cpp
@@ -1,3 +1,24 @@
+//O(LogN)
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int left = 1;
+        int right = n;
+        while(left <= right){
+            long mid = left + (right - left) / 2;
+            long currTiles =  mid * (mid + 1) / 2;
+
+            if(currTiles <= n){
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        return right;
+    }
+};
+
 // O(N)
 class Solution {
 public:
