0b9ef0c82082ba30461065a55d3d612c25d13304
Bhushan Gawde <bhushangawde32@gmail.com>
2023-07-28 15:20:36 +0530

    Binary search simpler solution

--- 540_Single_element_in_sorted_array.cpp
+++ 540_Single_element_in_sorted_array.cpp
@@ -1,3 +1,46 @@
+// Simple to understand solution
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int size = nums.size();
+        if(size == 1 || nums[0] != nums[1])
+            return nums[0];
+
+        if(nums[size - 1] != nums[size - 2])
+            return nums[size - 1];
+
+        int start = 0;
+        int end = size - 1;
+
+        while(start <= end){
+            int mid = (start + end) / 2;
+
+            if(nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1])
+                return nums[mid];
+
+            if(mid % 2 == 0){
+                if(nums[mid] == nums[mid + 1]){
+                    start = mid + 1;
+                }
+                else {
+                    end = mid - 1;
+                }
+            }
+            else {
+                if(nums[mid - 1] == nums[mid]){
+                    start = mid + 1;
+                }
+                else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+
+// Solution 1
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& arr) {
