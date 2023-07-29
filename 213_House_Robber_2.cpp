988817c039811e3580a7c8bcc36c7a028c73d352
Bhushan Gawde <bhushangawde32@gmail.com>
2023-07-29 21:57:10 +0530

    Bottom up dp

--- 213_House_Robber_2.cpp
+++ 213_House_Robber_2.cpp
@@ -1,3 +1,31 @@
+// Using bottom up - supports both arrays
+class Solution {
+public:
+    int solve(vector<int> &nums, int idx, vector<int>&dp, int n){
+        dp[0] = 0;
+        dp[1] = nums[idx];
+
+        int dpidx = 2;
+
+        for(int i = idx + 1; i < n; i++){
+            dp[dpidx] = max(dp[dpidx - 1], dp[dpidx - 2] + nums[i]);
+            dpidx++;
+        }
+
+        return dp[dpidx - 1];
+    }
+
+    int rob(vector<int>& nums) {
+        if(nums.size() == 1)
+            return nums[0];
+
+        vector<int> dp1(nums.size() + 2, -1);    
+        vector<int> dp2(nums.size() + 2, -1);    
+
+        return max(solve(nums, 0, dp1, nums.size() - 1), solve(nums, 1, dp2, nums.size()));
+    }
+};
+
 // Using Memoization
 class Solution {
 public:
