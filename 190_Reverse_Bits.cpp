f4f3aa4fbc5f4a50b8a0fd8e48f292685253833b
Bhushan Gawde <bhushangawde32@gmail.com>
2023-08-02 11:47:18 +0200

    Simple bit manipulation

---
+++ 190_Reverse_Bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans = 0;
+        for(int i = 0 ; i < 31; i++){
+            ans = ans | (n & 1);
+            n = n >> 1;
+            ans = ans << 1;
+        }
+        return ans | (n & 1);
+    }
+};
