f72adb484c3a806865b749bc4fe5c192341847ee
Bhushan Gawde <bhushangawde32@gmail.com>
2023-08-02 11:46:24 +0200

    Solution 3

--- 191_Number_of_1bits.cpp
+++ 191_Number_of_1bits.cpp
@@ -1,4 +1,17 @@
 
+// Solution 3
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int c = 0;
+        while(n > 0) {
+            c += (n & 1);
+            n >>= 1;
+        }
+        return c;
+    }
+};
+
 // Another solution
 class Solution {
 public:
