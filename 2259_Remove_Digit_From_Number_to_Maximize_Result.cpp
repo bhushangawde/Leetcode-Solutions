a47611b2949b3e962b70131b9a0a7118b0630652
Bhushan Gawde <bhushangawde32@gmail.com>
2023-08-03 02:09:11 +0530

    Using substr

--- 2259_Remove_Digit_From_Number_to_Maximize_Result.cpp
+++ 2259_Remove_Digit_From_Number_to_Maximize_Result.cpp
@@ -1,3 +1,32 @@
+// Using substr()
+class Solution {
+public:
+    string removeDigit(string number, char digit) {
+        int maxDiff = 0;
+        int pos = -1;
+        int size = number.size();
+        for(int i = 0 ; i < size - 1; i++){
+            if(number[i] == digit && number[i + 1] > digit){
+                pos = i;
+                break;
+            }
+        }
+
+        if(pos == -1){
+            for(int i = size - 1; i >= 0; i--){
+                if(number[i] == digit){
+                    pos = i;
+                    break;
+                }
+            }
+        }
+        
+        string answer = number.substr(0, pos) + number.substr(pos + 1);
+
+        return answer;
+    }
+};
+
 
 // Straightforward
 class Solution {
