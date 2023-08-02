8ba14ca1ef1a7deb35fdb1622fd4c25d2232892b
Bhushan Gawde <bhushangawde32@gmail.com>
2023-08-03 02:12:18 +0530

    Shorter solution - single loop

--- 2259_Remove_Digit_From_Number_to_Maximize_Result.cpp
+++ 2259_Remove_Digit_From_Number_to_Maximize_Result.cpp
@@ -1,3 +1,24 @@
+// Shorter solution
+class Solution {
+public:
+    string removeDigit(string number, char digit) {
+        int maxDiff = 0;
+        int pos = -1;
+        int size = number.size();
+        for(int i = 0 ; i < size; i++){
+            if(number[i] == digit){ 
+                pos = i;
+                if(i < size - 1 && number[i + 1] > digit){
+                    break;
+                }
+            }
+        }
+        string answer = number.substr(0, pos) + number.substr(pos + 1);
+        return answer;
+    }
+};
+
+
 // Using substr()
 class Solution {
 public:
