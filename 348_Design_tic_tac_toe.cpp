4298016e22580c794e42e6719fe1d797520939e3
Bhushan Gawde <bhushangawde32@gmail.com>
2023-08-04 15:38:42 +0530

    Simplified move() function

--- 348_Design_tic_tac_toe.cpp
+++ 348_Design_tic_tac_toe.cpp
@@ -1,3 +1,42 @@
+// Simplied solution
+class TicTacToe {
+public:
+    unordered_map<int, int> rowMp, colMp;
+    int diag, revDiag;
+    int num;
+    TicTacToe(int n) {
+        num = n;
+        diag = 0;
+        revDiag = 0;
+    }
+    
+    int move(int row, int col, int player) {
+        int currPlayer = (player == 1) ? 1 : -1;
+
+        rowMp[row] += currPlayer;
+        colMp[col] += currPlayer;
+        if(row == col){
+            diag += currPlayer;
+        }
+        if(row + col == num - 1){
+            revDiag += currPlayer;
+        }
+        
+        if(abs(diag) == num || abs(revDiag) == num || abs(rowMp[row]) == num || abs(colMp[col]) == num){
+            return player;
+        }
+
+        return 0;
+    }
+};
+
+/**
+ * Your TicTacToe object will be instantiated and called as such:
+ * TicTacToe* obj = new TicTacToe(n);
+ * int param_1 = obj->move(row,col,player);
+ */
+
+// First solution
 class TicTacToe {
 public:
     unordered_map<int, int> rowMp, colMp;
