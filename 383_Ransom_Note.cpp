d9eb9594cafd9764983b5213abdda2bd5f873636
Bhushan Gawde <bhushangawde32@gmail.com>
2023-08-05 03:49:13 +0530

    Unordered map

---
+++ 383_Ransom_Note.cpp
@@ -0,0 +1,36 @@
+// Shorter solution
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> mp;
+        for(auto s : magazine)
+            mp[s]++;
+        
+        for(auto s : ransomNote){
+            mp[s]--;
+            if(mp[s] == -1)
+                return false;
+        }
+        return true;
+    }
+};
+
+
+// Map
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> mp;
+        for(auto s : magazine)
+            mp[s]++;
+        
+        for(auto s : ransomNote)
+            mp[s]--;
+
+        for(auto e : mp){
+            if(e.second < 0)
+                return false;
+        }
+        return true;
+    }
+};
