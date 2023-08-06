438850d7f1358ff387323e578af82c1c263f1512
Bhushan Gawde <bhushangawde32@gmail.com>
2023-08-06 16:04:23 +0530

    Bottom up O(N) simplified

--- 2361_Minimum_Costs_Using_the_Train_Line.cpp
+++ 2361_Minimum_Costs_Using_the_Train_Line.cpp
@@ -1,3 +1,31 @@
+// Bottom up simplified solution
+class Solution {
+public:
+    vector<long long> minimumCosts(vector<int>& regular, vector<int>& express, int expressCost) {
+        int stops = regular.size() + 1;
+        vector<vector<long long>> dp(stops, vector<long long>(2, 0));
+        vector<long long> ans;
+        dp[0][0] = 0;
+        dp[0][1] = expressCost;
+
+
+        for(int i = 1; i < stops; i++){
+            long long reg_reg = dp[i - 1][0] + regular[i - 1];
+            long long exp_reg = dp[i - 1][1] + regular[i - 1];
+            dp[i][0] = min(reg_reg, exp_reg);
+
+
+            long long reg_exp = dp[i - 1][0] + expressCost + express[i - 1];  
+            long long exp_exp = dp[i - 1][1] + express[i - 1];
+            dp[i][1] = min(reg_exp, exp_exp);
+
+            ans.push_back(min(dp[i][1], dp[i][0]));
+        }
+
+        return ans;
+    }
+};
+
 
 
 // Bottom up solution
