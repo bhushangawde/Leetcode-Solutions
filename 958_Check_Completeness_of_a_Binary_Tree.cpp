0bddde60ad61fc753938a3ecf297398e0a319a1f
Bhushan Gawde <bhushangawde32@gmail.com>
2023-08-08 12:21:44 +0530

    Shorter solution

--- 958_Check_Completeness_of_a_Binary_Tree.cpp
+++ 958_Check_Completeness_of_a_Binary_Tree.cpp
@@ -1,3 +1,48 @@
+// Shorter solution
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isCompleteTree(TreeNode* root) {
+        if(!root)
+            return true;
+        queue<TreeNode*> q;
+        q.push(root);
+        bool nullFound = false;
+        while(!q.empty()){
+            int size = q.size();
+            for(int i = 0 ; i < size; i++){
+                TreeNode* node = q.front();
+                q.pop();
+
+                if(!node){
+                    nullFound = true;
+                }
+                else {
+                    if(nullFound){
+                        return false;
+                    }
+                    q.push(node->left);
+                    q.push(node->right);
+                }
+            }
+        }
+        return true;
+    }
+};
+
+
+
+
 // Level Order Traversal
 /**
  * Definition for a binary tree node.
